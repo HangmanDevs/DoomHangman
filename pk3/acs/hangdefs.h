153f3478ffcdcb7f08be6c1e87eee39677c796cc
Synert <synert@gmail.com>
2013-03-24 02:03:33 +0000

    Added sector color changing stuff, also niggers

--- pk3/acs/hangdefs.h
+++ pk3/acs/hangdefs.h
@@ -1,4 +1,12 @@
 #define HANGMAN_WORDOPEN        481
 #define HANGMAN_CHOOSEWORD      482
+#define SETCOLORS 900
 
 global int 32:HangmanWord[];
+
+script SETCOLORS (int r, int g, int b) clientside {
+	for(int s = 0; s < 32767; s++)
+	{
+		Sector_SetColor(s, r, g, b);
+	}
+}
